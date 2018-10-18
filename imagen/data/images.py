6b5a85e657cad96446e6e919346e7f7bb9a20606
Alex J. Champandard <alexjc@aigamedev.com>
2018-10-18 19:31:47 +0200

    Loading and saving images with PIL, converting to pseudo-normalized tensors with RGB components in the range -2.0 +2.0.

---
+++ imagen/data/images.py
@@ -0,0 +1,19 @@
+# Neural Imagen — Copyright (c) 2018, Alex J. Champandard. Code licensed under the GNU AGPLv3.
+
+import torch
+import torchvision.transforms as transforms
+import PIL.Image
+
+
+def load_from_file(filename, device):
+    loader = transforms.Compose([transforms.ToTensor()])
+    image = PIL.Image.open(filename)
+    image = loader(image).unsqueeze(0)
+    return image.to(device, torch.float) * 4.0 - 2.0
+
+
+def save_to_file(image, filename):
+    img = (image * 0.25 + 0.5).mul(255.0).clamp(0, 255.0).detach().cpu().numpy()
+    img = img[0].transpose(1, 2, 0).astype("uint8")
+    img = PIL.Image.fromarray(img)
+    img.save(filename)
