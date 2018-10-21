c9bcdad87769aee9f7da2525674a035e67750d91
Alex J. Champandard <alexjc@aigamedev.com>
2018-10-21 11:00:15 +0200

    Using average pool by default as this benefits generative models.

--- imagen/models/classifiers.py
+++ imagen/models/classifiers.py
@@ -16,12 +16,19 @@ class VGG19(torch.nn.Module):
 
         self.load_state_dict(torch.load('data/vgg19_torch.pth'))
 
+        for i, f in enumerate(self.features):
+            if isinstance(f, torch.nn.MaxPool2d):
+                self.features[i] = torch.nn.AvgPool2d(f.kernel_size, f.stride)
+
         self.stmean = torch.tensor([0.485, 0.456, 0.406]).view(1,3,1,1)
         self.stddev = torch.tensor([0.229, 0.224, 0.225]).view(1,3,1,1)
 
     def extract(self, image, layers : set): # {1, 6, 11, 20, 29}:
         """Preprocess an image to be compatible with pre-trained model, and return required features.
         """
+        if len(layers) == 0:
+            raise StopIteration
+
         image = ((image * 0.25 + 0.5) - self.stmean) / self.stddev
         for i in range(max(layers)+1):
             image = self.features[i].forward(image)
