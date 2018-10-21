cdd3ba280bf47442bd958841d50a1aa54a9c0284
Alex J. Champandard <alexjc@aigamedev.com>
2018-10-21 13:04:58 +0200

    Configuration of layer weights from the command-line.

--- examples/iterative.py
+++ examples/iterative.py
@@ -13,6 +13,9 @@ from imagen.models import classifiers
 class StyleTransfer(optim.ImageOptimizer):
 
     def __init__(self, args):
+        if args.random_seed is not None:
+            torch.manual_seed(args.seed)
+
         self.device = torch.device(args.device)
         self.model = classifiers.VGG19().to(self.device)
 
@@ -23,27 +26,26 @@ class StyleTransfer(optim.ImageOptimizer):
         super().__init__(seed_img, evaluations=args.iterations)
 
         self.config = args
-        self.all_layers = self.config.content_layers | self.config.style_layers
+        self.config.style_weights = [w * self.config.style_multiplier for w in self.config.style_weights]
+        self.all_layers = set(self.config.content_layers) | set(self.config.style_layers)
 
         self.content_feat = {k: (v - 1.0).detach() for k, v in self.model.extract(content_img, layers=self.config.content_layers)}
         self.style_feat = {k: self.model.gram_matrix(v - 1.0).detach() for k, v in self.model.extract(style_img, layers=self.config.style_layers)}
 
     def evaluate(self, image):
-        style_score = 0.0
-        content_score = 0.0
+        style_score = torch.tensor(0.0)
+        content_score = torch.tensor(0.0)
 
+        cw, sw = iter(self.config.content_weights), iter(self.config.style_weights)
         for i, l in self.model.extract(image, layers=self.all_layers):
             if i in self.config.content_layers:
-                content_score += F.mse_loss(self.content_feat[i], l - 1.0)
+                content_score += F.mse_loss(self.content_feat[i], l - 1.0) * next(cw)
             if i in self.config.style_layers:
-                style_score += F.mse_loss(self.style_feat[i], self.model.gram_matrix(l - 1.0))
+                style_score += F.mse_loss(self.style_feat[i], self.model.gram_matrix(l - 1.0)) * next(sw)
 
-        style_score *= self.config.style_weight / len(self.style_feat)
-        content_score *= self.config.content_weight
-
-        if self.should_do(self.config.save_every) == 0:
-            images.save_to_file(self.image, 'output/test%03i.png' % self.counter)
-        if self.should_do(self.config.print_every) == 0:
+        if self.should_do(self.config.save_every):
+            images.save_to_file(self.image.clone().detach().cpu(), 'output/test%03i.png' % self.counter)
+        if self.should_do(self.config.print_every):
             print('Iteration: {}    Style Loss: {:4f}     Content Loss: {:4f}'.format(
                 self.counter, style_score.item(), content_score.item()))
 
@@ -61,11 +63,13 @@ def main(args):
     add_arg('content', type=str, help='Image to use as reference.')
     add_arg('--with', dest='style', type=str, required=True, help='Image for inspiration.')
     add_arg('--iterations', type=int, default=250, help='Number of iterations.')
+    add_arg('--random-seed', type=int, default=None, help='Seed for random numbers.')
     add_arg('--device', type=str, default=device, help='Where to perform the computation.')
-    add_arg('--style-layers', type=set, default={1, 6, 11, 20})
-    add_arg('--style-weight', type=float, default=100000.0)
-    add_arg('--content-layers', type=set, default={20})
-    add_arg('--content-weight', type=float, default=1.0)
+    add_arg('--style-layers', type=int, nargs='*', default=[1, 6, 11, 20])
+    add_arg('--style-weights', type=float, nargs='*', default=[1.0, 1.0, 1.0, 1.0])
+    add_arg('--style-multiplier', type=float, default=1e+6)
+    add_arg('--content-layers', type=int, nargs='*', default=[20])
+    add_arg('--content-weights', type=float, nargs='*', default=[1.0])
     add_arg('--save-every', type=int, default=0)
     add_arg('--print-every', type=int, default=10)
     args = parser.parse_args()
