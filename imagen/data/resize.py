b7974d8adc6df6afd6304d748cd0771ecbbf748f
Alex J. Champandard <alexjc@aigamedev.com>
2018-10-21 11:04:20 +0200

    Resize operations now assume input tensors of (B,C,H,W) — whether images or patches.

---
+++ imagen/data/resize.py
@@ -0,0 +1,34 @@
+# Neural Imagen — Copyright (c) 2018, Alex J. Champandard. Code licensed under the GNU AGPLv3.
+
+import torch
+import torch.nn
+
+
+class DownscaleBuilder:
+
+    def __init__(self, factor, channels=3):
+        self.downscaler = torch.nn.Sequential(
+            torch.nn.Conv2d(channels, channels, groups=channels,
+                            kernel_size=(factor, factor), stride=(factor, factor))
+        )
+
+        self.downscaler[0].bias[:] = 0.0
+        self.downscaler[0].weight[:,0,:,:] = 1.0 / (factor ** 2)
+
+    def build(self, image):
+        with torch.no_grad():
+            result = self.downscaler(image)
+        return result
+
+
+class UpscaleBuilder:
+
+    def __init__(self, factor, channels=3, mode='nearest'):
+        self.upscaler = torch.nn.Sequential(
+            torch.nn.Upsample(scale_factor=(factor, factor), mode=mode)
+        )
+
+    def build(self, image):
+        with torch.no_grad():
+            result = self.upscaler(image)
+        return result
