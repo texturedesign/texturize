87fd28052847d5c5897628c28ef3fb28cb4845f3
Alex J. Champandard <alexjc@aigamedev.com>
2018-10-21 11:10:20 +0200

    Added out-of-range loss when the values in the image diverge, since there's no way to specify the allowed range with PyToch's implementation of L-BFGS.

--- imagen/optim.py
+++ imagen/optim.py
@@ -1,6 +1,7 @@
 # Neural Imagen — Copyright (c) 2018, Alex J. Champandard. Code licensed under the GNU AGPLv3.
 
 import torch.optim
+import torch.nn.functional as F
 
 
 class ImageOptimizer:
@@ -8,7 +9,7 @@ class ImageOptimizer:
     def __init__(self, image, evaluations):
         self.evaluations = evaluations
         self.counter = 0
-        self.image = image.detach_().requires_grad_()
+        self.image = image.detach().requires_grad_()
         self.lbfgs = torch.optim.LBFGS([self.image])
 
     def run(self):
@@ -20,13 +21,16 @@ class ImageOptimizer:
 
     def tick(self):
         self.lbfgs.zero_grad()
-        self.image.data.clamp_(-2.0, +2.0)
         
-        loss = self.evaluate(self.image)
+        image = self.image.clamp(-2.0, +2.0)
+        loss = self.evaluate(image)
         loss.backward()
 
+        out_of_range_loss = F.mse_loss(image, self.image)
+        self.image.data.clamp_(-2.0, +2.0)
+
         self.counter += 1
-        return loss
+        return loss + out_of_range_loss
 
     def evaluate(self, image):
         raise NotImplementedError
