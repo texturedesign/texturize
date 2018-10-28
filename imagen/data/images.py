993625d31601dc52a78b6b78b9bc90f44a9c404d
Alex J. Champandard <alexjc@aigamedev.com>
2018-10-28 22:10:18 +0100

    Support to convert images to other color profiles with PIL, e.g. YCbCr.

--- imagen/data/images.py
+++ imagen/data/images.py
@@ -5,15 +5,15 @@ import torchvision.transforms as transforms
 import PIL.Image
 
 
-def load_from_file(filename, device):
+def load_from_file(filename, device, mode='RGB'):
     loader = transforms.Compose([transforms.ToTensor()])
-    image = PIL.Image.open(filename)
+    image = PIL.Image.open(filename).convert(mode)
     image = loader(image).unsqueeze(0)
     return image.to(device, torch.float) * 4.0 - 2.0
 
 
-def save_to_file(image, filename):
+def save_to_file(image, filename, mode='RGB'):
     img = (image * 0.25 + 0.5).mul(255.0).clamp(0, 255.0).detach().cpu().numpy()
-    img = img[0].transpose(1, 2, 0).astype("uint8")
-    img = PIL.Image.fromarray(img)
+    img = img[0].transpose(1, 2, 0).astype('uint8')
+    img = PIL.Image.fromarray(img, mode)
     img.save(filename)
