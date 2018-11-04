388d6fddd02c79c8061a38b998fb152f2b1ebb89
Alex J. Champandard <alexjc@aigamedev.com>
2018-11-04 15:31:05 +0100

    Adding setuptools file for packaging and installation.

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name="neural-imagen",
+    version="0.1.dev0",
+    author="Alex J. Champandard",
+    description="Image synthesis using deep neural networks.",
+    license="AGPLv3+",
+    keywords="deeplearning graphics",
+    url="https://github.com/alexjc/neural-imagen",
+    packages=['imagen'],
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Topic :: Utilities",
+        "Topic :: Multimedia :: Graphics",
+        "Topic :: Software Development :: Libraries",
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+    ],
+)
