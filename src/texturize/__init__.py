d70945c93d5069fae37e4cfa73bb32e9d468fadd
Alex J. Champandard <445208+alexjc@users.noreply.github.com>
2020-06-11 19:16:30 +0200

    Disable poetry's nested virtualenv creation.

--- src/texturize/__init__.py
+++ src/texturize/__init__.py
@@ -1,3 +1,10 @@
 # neural-texturize — Copyright (c) 2020, Novelty Factory KG.  See LICENSE for details.
 
 __version__ = "0.4"
+
+
+try:
+    import torch
+except:
+    import sys
+    print(sys.path)
